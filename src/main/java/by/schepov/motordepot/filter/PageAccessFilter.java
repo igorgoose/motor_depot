6bb522bb1230e6f30b52a6bf03385b6c60c4735c
igorgoose <igotgood01@gmail.com>
2020-05-14 18:52:55 +0300

    Add PageAccessFilter

--- src/main/java/by/schepov/motordepot/filter/PageAccessFilter.java
+++ src/main/java/by/schepov/motordepot/filter/PageAccessFilter.java
@@ -1,10 +1,12 @@
 package by.schepov.motordepot.filter;
 
-import by.schepov.motordepot.command.Command;
 import by.schepov.motordepot.entity.Role;
 import by.schepov.motordepot.entity.User;
 import by.schepov.motordepot.entity.UserStatus;
-import by.schepov.motordepot.parameter.*;
+import by.schepov.motordepot.parameter.MessageKey;
+import by.schepov.motordepot.parameter.Page;
+import by.schepov.motordepot.parameter.RequestAttribute;
+import by.schepov.motordepot.parameter.SessionAttribute;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,30 +16,27 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class AccessSecurityFilter implements Filter {
+public class PageAccessFilter implements Filter {
 
-    private static final Logger LOGGER = LogManager.getLogger(AccessSecurityFilter.class);
+    private static final Logger LOGGER = LogManager.getLogger(PageAccessFilter.class);
     private static final String BUNDLE_NAME = "locale";
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String commandName = request.getParameter(JSPParameter.COMMAND.getName());
+        User user = (User) request.getSession().getAttribute(SessionAttribute.USER.getName());
+        String uri = request.getRequestURI();
+        if(user != null && user.getStatus() == UserStatus.BLOCKED){
+            LOGGER.warn("Blocked user tried to access " + uri + "; user: " + user);
+            setMessage(request, MessageKey.YOU_ARE_BLOCKED);
+            request.getRequestDispatcher(Page.ERROR.getName()).forward(servletRequest, servletResponse);
+            return;
+        }
         Role role = (Role) request.getSession().getAttribute(SessionAttribute.ROLE.getName());
-        if(commandName != null){
-            Command command = Command.getCommandByName(commandName);
-            if(role != Role.GUEST){
-                User user = (User) request.getSession().getAttribute(SessionAttribute.USER.getName());
-                if(user.getStatus() == UserStatus.BLOCKED){
-                    LOGGER.info("Command(" + command.getName() + ") was accessed by a blocked user!");
-                    setMessage(request, MessageKey.YOU_ARE_BLOCKED);
-                    request.getSession().invalidate();
-                    request.getRequestDispatcher(Page.ERROR.getName()).forward(servletRequest, servletResponse);
-                    return;
-                }
-            }
-            if(!command.getAccessChecker().canBeAccessedBy(role)){
-                LOGGER.error("Command(" + command.getName() + ") was accessed by a user with low priority(" + role + ")!");
+        String contextPath = request.getContextPath();
+        for (Page page : Page.values()) {
+            if(uri.equals(contextPath + page.getName()) && !page.getAccessChecker().canBeAccessedBy(role)){
+                LOGGER.warn("Page(" + uri + ") was accessed by a user with low priority(" + role + ")!");
                 setMessage(request, MessageKey.ACCESS_DENIED);
                 request.getRequestDispatcher(Page.ERROR.getName()).forward(servletRequest, servletResponse);
                 return;
@@ -46,10 +45,10 @@ public class AccessSecurityFilter implements Filter {
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
-
     private void setMessage(HttpServletRequest request, MessageKey messageKey){
         Locale locale = new Locale((String)request.getSession().getAttribute(SessionAttribute.LOCALE.getName()));
         ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, locale);
         request.setAttribute(RequestAttribute.MESSAGE.getName(), bundle.getString(messageKey.getValue()));
     }
+
 }
--- src/main/java/by/schepov/motordepot/parameter/Page.java
+++ src/main/java/by/schepov/motordepot/parameter/Page.java
@@ -1,29 +1,39 @@
 package by.schepov.motordepot.parameter;
 
+import by.schepov.motordepot.command.access.AccessChecker;
+import by.schepov.motordepot.command.access.impl.EqualRoleAccessChecker;
+import by.schepov.motordepot.command.access.impl.LessOrEqualRoleAccessChecker;
+import by.schepov.motordepot.entity.Role;
+
 public enum Page {
 
-    FINISH_ORDER("/jsp/functions/finish_order.jsp"),
-    REQUEST_VERIFICATION("/jsp/functions/request_verification.jsp"),
-    MANAGEMENT("/jsp/management/management.jsp"),
-    MANAGEMENT_REQUESTS("/jsp/management/management_requests.jsp"),
-    MANAGEMENT_USERS("/jsp/management/management_users.jsp"),
-    MANAGEMENT_CARS("/jsp/management/management_cars.jsp"),
-    MANAGEMENT_ORDERS("/jsp/management/management_orders.jsp"),
-    HOME("/index.jsp"),
-    AUTHORIZE("/jsp/authorize.jsp"),
-    SIGN_UP("/jsp/signup.jsp"),
-    PROFILE("/jsp/profile.jsp"),
-    ERROR("/jsp/error.jsp"),
-    USER_DETAILS("/jsp/functions/user_details.jsp"),
-    SUBMIT_ORDER("/jsp/functions/submit_order.jsp");
+    FINISH_ORDER("/jsp/functions/finish_order.jsp", new EqualRoleAccessChecker(Role.DRIVER)),
+    REQUEST_VERIFICATION("/jsp/functions/request_verification.jsp", new EqualRoleAccessChecker(Role.ADMIN)),
+    MANAGEMENT("/jsp/management/management.jsp", new LessOrEqualRoleAccessChecker(Role.DRIVER)),
+    MANAGEMENT_REQUESTS("/jsp/management/management_requests.jsp", new LessOrEqualRoleAccessChecker(Role.DRIVER)),
+    MANAGEMENT_USERS("/jsp/management/management_users.jsp", new EqualRoleAccessChecker(Role.ADMIN)),
+    MANAGEMENT_CARS("/jsp/management/management_cars.jsp", new LessOrEqualRoleAccessChecker(Role.DRIVER)),
+    MANAGEMENT_ORDERS("/jsp/management/management_orders.jsp", new LessOrEqualRoleAccessChecker(Role.DRIVER)),
+    HOME("/index.jsp", new LessOrEqualRoleAccessChecker(Role.GUEST)),
+    AUTHORIZE("/jsp/authorize.jsp", new LessOrEqualRoleAccessChecker(Role.GUEST)),
+    SIGN_UP("/jsp/signup.jsp", new LessOrEqualRoleAccessChecker(Role.GUEST)),
+    ERROR("/jsp/error.jsp", new LessOrEqualRoleAccessChecker(Role.GUEST)),
+    USER_DETAILS("/jsp/functions/user_details.jsp", new LessOrEqualRoleAccessChecker(Role.USER)),
+    SUBMIT_ORDER("/jsp/functions/submit_order.jsp", new EqualRoleAccessChecker(Role.ADMIN));
 
     private final String name;
+    private final AccessChecker accessChecker;
 
-    Page(String name){
+    Page(String name, AccessChecker accessChecker){
+        this.accessChecker = accessChecker;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public AccessChecker getAccessChecker() {
+        return accessChecker;
     }
 }
